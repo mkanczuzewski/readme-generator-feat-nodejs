6bc0dba8b7770f45e1c674d33fcbe09053ad3598
mkanczuzewski <mkanczuzewski@gmail.com>
2021-11-29 14:46:00 -0500

    Finish questions

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const promptQuestions = () => 
@@ -12,9 +11,9 @@ const promptQuestions = () =>
             type: 'input',
             name: 'projectName',
             message: 'Provide project name. (Required)',
-            validate: nameInput => 
+            validate: projectNameInput => 
             {
-                if (nameInput) 
+                if (projectNameInput) 
                 {
                     return true;
                 }   else 
@@ -22,12 +21,64 @@ const promptQuestions = () =>
                         console.log('Please enter your project name!');
                         return false;
                     }
-            },
-        }
+            }
+        },
+        {
+            type: 'input',
+            name: 'projectDesc',
+            message: 'Create project Description. (Required)',
+            validate: projectDescInput => 
+            {
+                if (projectDescInput) 
+                {
+                    return true;
+                }   else 
+                    {
+                        console.log('Please enter your project description!');
+                        return false;
+                    }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmToc',
+            message: 'Would you like to provide a table of contents?',
+            default: true  //need to handle this at Readme generation
+        },
+        {
+            type: 'input',
+            name: 'projectInst',
+            message: 'Input installation instructions.',
+        },
+        {
+            type: 'input',
+            name: 'projectUsage',
+            message: 'Discuss usage information.',
+        },
+        {
+            type: 'confirm',
+            name: 'confirmLic',
+            message: 'Would you like to include a template license?',
+        },
+        {
+            type: 'input',
+            name: 'projectCont',
+            message: 'Discuss contribution guidelines.',
+        },
+        {
+            type: 'input',
+            name: 'projectTest',
+            message: 'Review test instructions.',
+        },
+        {
+            type: 'input',
+            name: 'projectQuest',
+            message: 'Insert questions.',
+        },
     ]);
 }
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
