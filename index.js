c524ec0351839ceab656d27bfafa8a742c472cbd
mkanczuzewski <mkanczuzewski@gmail.com>
2021-11-29 11:48:47 -0500

    Question prompt POC

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const promptQuestions = () => 
+{
+    return inquirer.prompt
+    ([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'Provide project name. (Required)',
+            validate: nameInput => 
+            {
+                if (nameInput) 
+                {
+                    return true;
+                }   else 
+                    {
+                        console.log('Please enter your project name!');
+                        return false;
+                    }
+            },
+        }
+    ]);
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -10,4 +33,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+promptQuestions();
